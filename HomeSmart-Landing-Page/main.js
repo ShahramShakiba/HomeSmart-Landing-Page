f92d5770a62c78bff8cac224b28dd608c1680a1e
Shahram Shakiba <ShahramShakiba88@gmail.com>
2023-11-25 11:33:16 +0330

    Implementing functionality to the MenuBar

--- HomeSmart-Landing-Page/main.js
+++ HomeSmart-Landing-Page/main.js
@@ -1,2 +1,15 @@
-import './style.css'
+import './style.css';
 
+const navBtn = document.querySelector('#menu'),
+  menuBar = document.querySelector('[role="menuBar"]');
+
+navBtn.addEventListener('click', () => {
+  //parsed value of the "aria-expanded" attribute of the "navBtn"
+  const isExpanded = JSON.parse(navBtn.getAttribute('aria-expanded'));
+
+  //sets the "aria-expanded" of the "navBtn" to the opposite boolean value
+  //if it's true make it false and vice versa
+  navBtn.setAttribute('aria-expanded', !isExpanded);
+  menuBar.classList.toggle('hidden');
+  menuBar.classList.toggle('flex');
+});
