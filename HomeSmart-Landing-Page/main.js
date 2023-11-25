fd232779dd6fd407196415eafc94c75567521936
Shahram Shakiba <ShahramShakiba88@gmail.com>
2023-11-25 22:16:04 +0330

    Style the Header-section

--- HomeSmart-Landing-Page/main.js
+++ HomeSmart-Landing-Page/main.js
@@ -1,7 +1,7 @@
 import './style.css';
 
 const navBtn = document.querySelector('#menu'),
-  menuBar = document.querySelector('[role="menuBar"]');
+  menuBar = document.querySelector('[role="menubar"]');
 
 navBtn.addEventListener('click', () => {
   //parsed value of the "aria-expanded" attribute of the "navBtn"
@@ -10,6 +10,8 @@ navBtn.addEventListener('click', () => {
   //sets the "aria-expanded" of the "navBtn" to the opposite boolean value
   //if it's true make it false and vice versa
   navBtn.setAttribute('aria-expanded', !isExpanded);
+
+  //switch between hidden and flex 
   menuBar.classList.toggle('hidden');
   menuBar.classList.toggle('flex');
 });
